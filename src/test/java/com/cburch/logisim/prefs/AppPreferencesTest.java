27780c4bf80a9befb22edc292411a20b0d901ddd
Gavin Abbott <gavin.abbott@eagles.oc.edu>
2026-04-10 18:05:35 -0500

    Added where the color of the default color text changes color based off of the color picker and saves that said color. Also added test cases to check

--- src/test/java/com/cburch/logisim/prefs/AppPreferencesTest.java
+++ src/test/java/com/cburch/logisim/prefs/AppPreferencesTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.awt.Color;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// 1. THIS IS THE MISSING WRAPPER:
 public class AppPreferencesTest {
 
     @Test
@@ -13,7 +12,6 @@ public class AppPreferencesTest {
 
         int actualStartingColor = AppPreferences.DEFAULT_TEXT_COLOR.get();
 
-        // 2. THIS IS THE FIXED LINE WITH .get():
         int expectedStartingColor = AppPreferences.DEFAULT_COMPONENT_ICON_COLOR;
 
         assertEquals(expectedStartingColor, actualStartingColor,
@@ -21,7 +19,6 @@ public class AppPreferencesTest {
 
         Color neonGreen = new Color(57, 255, 20);
 
-        // Save it to your preference
         AppPreferences.DEFAULT_TEXT_COLOR.set(neonGreen.getRGB());
 
         int updatedColor = AppPreferences.DEFAULT_TEXT_COLOR.get();
@@ -30,5 +27,4 @@ public class AppPreferencesTest {
                 "The preference should successfully save and return a newly selected color.");
     }
 
-// 3. DON'T FORGET THIS CLOSING BRACKET FOR THE CLASS!
 }
