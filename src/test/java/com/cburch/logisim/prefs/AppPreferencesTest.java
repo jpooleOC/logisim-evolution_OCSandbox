1d65fc03bdad03871605db760c7cf0f1fec2657a
Gavin Abbott <gavin.abbott@eagles.oc.edu>
2026-04-10 18:03:27 -0500

    Added test case where it gets the color that was saved and makes sure that it is the color that it is suppose to be

--- src/test/java/com/cburch/logisim/prefs/AppPreferencesTest.java
+++ src/test/java/com/cburch/logisim/prefs/AppPreferencesTest.java
@@ -1,23 +1,34 @@
 package com.cburch.logisim.prefs;
 
+import com.cburch.logisim.prefs.AppPreferences;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import java.awt.Color;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// 1. THIS IS THE MISSING WRAPPER:
 public class AppPreferencesTest {
 
     @Test
     public void testDefaultTextColorPreference() {
 
-        Integer initialColor = AppPreferences.DEFAULT_TEXT_COLOR.get();
-        assertEquals(Color.BLACK.getRGB(), initialColor, "The default text color should initialize to Black.");
+        int actualStartingColor = AppPreferences.DEFAULT_TEXT_COLOR.get();
 
-        Integer newColorToTest = Color.RED.getRGB();
-        AppPreferences.DEFAULT_TEXT_COLOR.set(newColorToTest);
+        // 2. THIS IS THE FIXED LINE WITH .get():
+        int expectedStartingColor = AppPreferences.DEFAULT_COMPONENT_ICON_COLOR;
 
-        Integer updatedColor = AppPreferences.DEFAULT_TEXT_COLOR.get();
-        assertEquals(newColorToTest, updatedColor, "The preference should update to the new color.");
+        assertEquals(expectedStartingColor, actualStartingColor,
+                "The Default Text Color should initially match the Component Icon Color.");
 
-        AppPreferences.DEFAULT_TEXT_COLOR.set(Color.BLACK.getRGB());
+        Color neonGreen = new Color(57, 255, 20);
+
+        // Save it to your preference
+        AppPreferences.DEFAULT_TEXT_COLOR.set(neonGreen.getRGB());
+
+        int updatedColor = AppPreferences.DEFAULT_TEXT_COLOR.get();
+
+        assertEquals(neonGreen.getRGB(), updatedColor,
+                "The preference should successfully save and return a newly selected color.");
     }
+
+// 3. DON'T FORGET THIS CLOSING BRACKET FOR THE CLASS!
 }
